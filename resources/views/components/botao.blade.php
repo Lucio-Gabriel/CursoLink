484b3a38439fdc11168825907ae3d7d43c848484
Gabriel <suppershock777@gmail.com>
2024-08-30 19:32:39 -0300

    Feat: Criado a primeira tela

---
+++ resources/views/components/botao.blade.php
@@ -0,0 +1,5 @@
+<a href="{{$url}}" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: #7C3BD9;
+    background-color: white; text-align: center; text-decoration: none;
+    border-radius: 5px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; font-size: 30px">
+    {{$slot}}
+</a>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>CursoLink</title>
+
 </head>
 <body>
+    <style>
+        body{
+            background-color: #7C3BD9;
+        }
 
-    <h1>Aqui minha primeira pagina</h1>
+        .title{
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            font-size: 50px;
+            color: white
+        }
+    </style>
+
+    <div style="display: flex; justify-content: center; flex-direction: column; align-items: center; height: 80vh;">
+        <div>
+          <img src=" {{ asset('img/logoCursoLink.png') }} " alt="logocurso">
+        </div>
+        <h1 class="title">Bem Vindos a Melhor
+            Plataforma de Cursos Grátis</h1>
+
+            @component('components.botao', ['url' => url('/cursos')])
+                Vamos lá
+            @endcomponent
+      </div>
+
+
 
 </body>
 </html>
